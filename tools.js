a3f04a0f81ab9b1a956365c4b4ce5f11feea368e
taliu <taliu@outlook.com>
2014-08-08 16:23:49 +0800

    完善tools 里Log日志对象

--- tools.js
+++ tools.js
@@ -1,14 +1,17 @@
 var Log={
 	info:function(msg){
-		console.log(msg);	
+		this._log("info",msg);		
 	},
 	debug:function(msg){
-		console.log(msg);	
+		this._log("debug",msg);	
 	},
 	error:function(msg){
-		console.log(msg);	
+		this._log("error",msg);		
 	},
         warn:function(msg){
-		console.log(msg);	
+		this._log("warn",msg);	
+	},
+	_log:function(logType,msg){
+		console.log("["+new Date().toString()+"]---"+logType.toUpperCase()+":"+msg);
 	}
 };
